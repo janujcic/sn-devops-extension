a7fd72c8a492e7f8cbff31d661921a41e5e9ba0a
jujcic <jan.ujcic@parsek.com>
2024-06-16 20:24:13 +0200

    Changed the way DOM is scanned for the right element, added an experimental function to add a summary (WIP).

--- main.js
+++ main.js
@@ -15,64 +15,200 @@ function generateServiceNowLink(fileName, baseServiceNowUrl) {
 // Function to inject the ServiceNow link into the Azure DevOps page
 function addServiceNowLink(baseServiceNowUrl) {
   // Check for the specific element where you want to add the link (e.g., file list, commit details)
+  const classElement = ".flex-grow.absolute-fill.repos-changes-viewer.flex-column.rhythm-vertical-16.scroll-auto.scroll-auto-hide.custom-scrollbar.is-folder";
+  const folderElement = document.querySelector(classElement); // Adjust the selector as needed
+  if (folderElement) {
+	const commitElement = ".repos-summary-header.flex-noshrink.bolt-card.flex-column.depth-8.bolt-card-white";
+	const childElements = folderElement.querySelectorAll(commitElement);
+
+	if (childElements) {
+		childElements.forEach(childElement => {
+			// Select the nested elements
+			const flexRow = childElement.querySelector('.flex-row');
+			const cardContent = childElement.querySelector('.bolt-card-content.flex-row.flex-grow.bolt-default-horizontal-spacing');
+			let serviceNowLink, docSysId, fileTitle, xmlSummary, fileName;
+			
+			if (flexRow) {
+				fileTitle = flexRow.querySelector('span.text-ellipsis');
+				fileName = fileTitle.textContent.trim();
+				let fileNameParts = fileName.split(".");
+				if (fileNameParts[fileNameParts.length - 1] == "xml") {
+					[serviceNowLink, docSysId] = generateServiceNowLink(fileName, baseServiceNowUrl);
+				}
+			}
+			
+			if (cardContent) {
+				xmlSummary = extractXML(cardContent);
+			}
+			
+			
+			if (typeof fileTitle === 'undefined' || typeof docSysId === 'undefined') {
+				return;
+			} 
+			
+			// Check if the link already exists to prevent duplicate links
+			if (!childElement.querySelector(`.service-now-link_${docSysId}`)) {
+
+				// Create the link element
+				const linkElement = document.createElement('a');
+				linkElement.href = serviceNowLink;
+				linkElement.textContent = "View in ServiceNow";
+				linkElement.target = "_blank";
+				linkElement.className = `service-now-link_${docSysId}`;
+				linkElement.style.marginLeft = "10px"; // Adjust styling as needed
+
+				// Append the link to the file element
+				fileTitle.appendChild(linkElement);
+			}
+			if (!childElement.querySelector(`.document_summary_button_${docSysId}`) && xmlSummary != "" && false) {
+				// Create the "Summary" button 
+				const summaryButton = document.createElement('button');
+				summaryButton.textContent = "Summary";
+				summaryButton.className = `document_summary_button_${docSysId}`;
+				summaryButton.style.marginLeft = "10px";
+				summaryButton.style.borderRadius = "12px";
+				summaryButton.style.padding = "5px 10px";
+				summaryButton.style.cursor = "pointer";
+				summaryButton.style.backgroundColor = "#333"; // Dark background
+				summaryButton.style.color = "#fff"; // White text
+				summaryButton.style.border = "1px solid #555"; // Slightly lighter border
+				summaryButton.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.2)"; // Subtle shadow
+
+				summaryButton.addEventListener('click', (event) => {
+				openSummaryPopup(event, fileName, xmlSummary);
+				});
+
+				// Append the link to the file element
+				fileTitle.appendChild(summaryButton);
+			}
+			
+		});
+	}
+	}
+	else {
+		const fileElements = document.querySelectorAll('.body-m.text-ellipsis');
+		fileElements.forEach(fileElement => {
+			const fileName = fileElement.textContent.trim();
+			console.log(fileName);
+			let fileNameParts = fileName.split(".");
+			if (fileNameParts[fileNameParts.length - 1] != "xml") {
+				return;
+			}
+			const [serviceNowLink, docSysId] = generateServiceNowLink(fileName, baseServiceNowUrl);
+			
+			if (!document.querySelector(`.service-now-link_${docSysId}`)) {
+
+					// Create the link element
+					const linkElement = document.createElement('a');
+					linkElement.href = serviceNowLink;
+					linkElement.textContent = "View in ServiceNow";
+					linkElement.target = "_blank";
+					linkElement.className = `service-now-link_${docSysId}`;
+					linkElement.style.marginLeft = "10px"; // Adjust styling as needed
+
+					// Append the link to the file element
+					fileElement.appendChild(linkElement);
+			}
+		});
+		
+	}
+}
+ 
+  /*
   const fileElements = document.querySelectorAll('.body-m.text-ellipsis'); // Adjust the selector as needed
+  
   fileElements.forEach(fileElement => {
+	// get the XML data
+	const fileElements = document.querySelectorAll('.body-m.text-ellipsis');
+	
+	
     const fileName = fileElement.textContent.trim();
 	let fileNameParts = fileName.split(".");
 	if (fileNameParts[fileNameParts.length - 1] != "xml") {
 		return;
 	}
+	
+	
+	
     const [serviceNowLink, docSysId] = generateServiceNowLink(fileName, baseServiceNowUrl);
+	
 
     // Check if the link already exists to prevent duplicate links
     if (!fileElement.querySelector(`.service-now-link_${docSysId}`)) {
 
-      // Create the link element
-      const linkElement = document.createElement('a');
-      linkElement.href = serviceNowLink;
-      linkElement.textContent = "View in ServiceNow";
-      linkElement.target = "_blank";
-      linkElement.className = `service-now-link_${docSysId}`;
-      linkElement.style.marginLeft = "10px"; // Adjust styling as needed
+		// Create the link element
+		const linkElement = document.createElement('a');
+		linkElement.href = serviceNowLink;
+		linkElement.textContent = "View in ServiceNow";
+		linkElement.target = "_blank";
+		linkElement.className = `service-now-link_${docSysId}`;
+		linkElement.style.marginLeft = "10px"; // Adjust styling as needed
 
-      // Append the link to the file element
-      fileElement.appendChild(linkElement);
+		// Append the link to the file element
+		fileElement.appendChild(linkElement);
     }
 	if (!fileElement.querySelector(`.document_summary_button_${docSysId}`)) {
+		
+		
+	  	
 		// Create the "Summary" button 
-      const summaryButton = document.createElement('button');
-      summaryButton.textContent = "Summary";
-      summaryButton.className = `document_summary_button_${docSysId}`;
-      summaryButton.style.marginLeft = "10px";
-      summaryButton.style.borderRadius = "12px";
-      summaryButton.style.padding = "5px 10px";
-      summaryButton.style.cursor = "pointer";
-	  summaryButton.style.backgroundColor = "#333"; // Dark background
-      summaryButton.style.color = "#fff"; // White text
-      summaryButton.style.border = "1px solid #555"; // Slightly lighter border
-      summaryButton.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.2)"; // Subtle shadow
-	  
-	  summaryButton.addEventListener('click', (event) => {
-        openSummaryPopup(event, fileName);
-      });
+		const summaryButton = document.createElement('button');
+		summaryButton.textContent = "Summary";
+		summaryButton.className = `document_summary_button_${docSysId}`;
+		summaryButton.style.marginLeft = "10px";
+		summaryButton.style.borderRadius = "12px";
+		summaryButton.style.padding = "5px 10px";
+		summaryButton.style.cursor = "pointer";
+		summaryButton.style.backgroundColor = "#333"; // Dark background
+		summaryButton.style.color = "#fff"; // White text
+		summaryButton.style.border = "1px solid #555"; // Slightly lighter border
+		summaryButton.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.2)"; // Subtle shadow
 
-      // Append the link to the file element
-      fileElement.appendChild(summaryButton);
+		summaryButton.addEventListener('click', (event) => {
+		openSummaryPopup(event, fileName, xmlSummary);
+		});
+
+		// Append the link to the file element
+		fileElement.appendChild(summaryButton);
 	}
 	
   }); 
+  */
+
+
+function extractXML(htmlElement) {
+	let xmlValues = {};
+	// Select all elements containing the XML lines
+    const xmlElements = document.querySelectorAll('.repos-line-content.added , .repos-line-content.unchanged');
+	
+	// Initialize an array to store the XML lines
+    let xmlLines = [];
+    
+    // Loop through each element and extract its inner text
+    xmlElements.forEach(element => {
+		const text = element.innerText.replace('Plus', '').trim();
+		xmlLines.push(text);
+    });
+	
+	// Join the lines to form the complete XML string
+    let xmlContent = xmlLines.join('\n');
+    
+    // Return or log the complete XML string
+    console.log(xmlContent);
+    return xmlContent;
+
 }
 
 // Function to open a small pop-up window with the summary of the XML file
-function openSummaryPopup(event, fileName) {
+function openSummaryPopup(event, fileName, xmlSummary) {
   // Remove any existing pop-ups
   const existingPopup = document.querySelector('.summary-popup');
   if (existingPopup) {
     existingPopup.remove();
   }
-
+  xmlSummary = JSON.stringify(xmlSummary);
   // Generate summary content (this should be replaced with the actual summary generation logic)
-  const summaryContent = `Summary for ${fileName}`;
+  const summaryContent = `<b>Summary for ${fileName}</b><br><br>${xmlSummary}`;
 
   // Create the pop-up element
   const popup = document.createElement('div');
